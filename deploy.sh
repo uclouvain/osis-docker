c5b7dd316e8bc48fc7cc44ccb6173ce7fa9b9175
Aurélien De Ryck <aurelien.deryck@usaintlouis.be>
2019-10-23 09:10:28 +0200

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -3,16 +3,15 @@
 set -e
 
 echo "Init repositories"
-git submodule update --init --remote --recursive
+#git submodule update --init --remote --recursive
 
 echo "Copy docker files"
-cp -r ./docker-env/osis/.env ./osis/.env
-cp -r ./docker-env/osis/Dockerfile ./osis/Dockerfile
-cp -r ./docker-env/osis-portal/.env ./osis-portal/.env
-cp -r ./docker-env/osis-portal/Dockerfile ./osis-portal/Dockerfile
+cp -r ./osis/.env.docker ./osis/.env
+cp -r ./osis-portal/.env.docker ./osis-portal/.env
 
 echo "Build docker compose"
 docker-compose build
+
 echo "Run migrations"
 docker-compose run osis python manage.py migrate
 docker-compose run osis-portal python manage.py migrate
